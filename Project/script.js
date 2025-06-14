7cb1a4ea0ee4205696997bf43200ba357bb7321e
markus248247 <armada.paella_0x@icloud.com>
2025-06-14 14:28:08 +0200

    - document.body.classList.add und .remove durch document.body.classList.toggle ersetzt - Anchor-Element durch echten Button ersetzt - Button wechselt nach 0,5 Sekunden wieder auf die Ursprungsfarbe

--- Project/script.js
+++ Project/script.js
@@ -1,25 +1,28 @@
+// Dark-/Light-Mode initial setzen
 if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) {
   document.body.classList.add('dark-mode');
 } else {
   document.body.classList.add('light-mode');
 }
 
+// Auf Änderung des Systemmodus reagieren
 window.matchMedia('(prefers-color-scheme: dark)').addEventListener('change', event => {
-  if (event.matches) {
-    document.body.classList.add('dark-mode');
-    document.body.classList.remove('light-mode');
-  } else {
-    document.body.classList.add('light-mode');
-    document.body.classList.remove('dark-mode');
-  }
+  document.body.classList.toggle('dark-mode', event.matches);
+  document.body.classList.toggle('light-mode', !event.matches);
 });
 
 
+ // Button-Farbe bei Klick ändern. Nach 0,5 Sekunden wechselt die 
+ // Farbe zurück auf die Ursprungsfarbe
 const buttons = document.querySelectorAll(".Button");
 
 for (const button of buttons) {
   button.addEventListener("click", function () {
     button.style.backgroundColor = "rgb(140, 134, 170)";
+    setTimeout(() => {
+      button.style.backgroundColor = "";
+    }, 500); // Farbe nach 0,5 Sekunden zurücksetzen
   });
 }
-    
+
+
