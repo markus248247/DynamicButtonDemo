9ed341658f6ebce168bd29c3a4c81d7ff6d21ac8
markus248247 <armada.paella_0x@icloud.com>
2025-06-14 13:49:28 +0200

    - Button hinzugefügt, gestylt und per Flexbox mittig auf der Seite platziert - Bei Klick auf den Button ändert dieser die Farbe - Darkmode hinzugefügt: Beim Wechsel des Browsers in den Dark-/Light-Mode passt sich das Styling automatisch an

--- Project/script.js
+++ Project/script.js
@@ -1,5 +1,25 @@
-let test = "Test";
-
-for(let i=0; i<3; i++){
-    console.log(test);
+if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) {
+  document.body.classList.add('dark-mode');
+} else {
+  document.body.classList.add('light-mode');
 }
+
+window.matchMedia('(prefers-color-scheme: dark)').addEventListener('change', event => {
+  if (event.matches) {
+    document.body.classList.add('dark-mode');
+    document.body.classList.remove('light-mode');
+  } else {
+    document.body.classList.add('light-mode');
+    document.body.classList.remove('dark-mode');
+  }
+});
+
+
+const buttons = document.querySelectorAll(".Button");
+
+for (const button of buttons) {
+  button.addEventListener("click", function () {
+    button.style.backgroundColor = "rgb(140, 134, 170)";
+  });
+}
+    
